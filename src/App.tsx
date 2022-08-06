42358e305d92114ce130595177bac0fc35891e55
omit671 <omit671@aucklanduni.ac.nz>
2022-08-06 17:51:52 +1200

    Show board game title, description, and box art when queried

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ const BGG_THING_API_URL = "https://boardgamegeek.com/xmlapi2/thing";
 function App() {
   const [boardGameID, setBoardGameID] = useState("");
 
+  const [boardGameInfo, setBoardGameInfo] = useState<undefined | any>(undefined);
+
   return (
       <div>
         <h1>Board Game Search</h1>
@@ -24,17 +26,40 @@ function App() {
           <br />
           <button onClick={queryAPI}>Search</button>
         </div>
+
+        { boardGameInfo !== undefined ? (
+            <div>
+              <img src={boardGameInfo.image._text!} alt={ `${boardGameInfo.name._attributes.value} Box Art`} />
+
+              <h2>{ boardGameInfo.name._attributes.value }</h2>
+
+              <p>{ boardGameInfo.description._text }</p>
+            </div>
+        ) : (
+            <p>No such board game with id { boardGameID }</p>
+        )}
       </div>
   );
 
   async function queryAPI() {
-    const response = await axios.get(BGG_THING_API_URL + "?id=" + boardGameID);
+    try
+    {
+      const response = await axios.get(BGG_THING_API_URL + "?id=" + boardGameID);
 
-    const data = JSON.parse(xml2json(response.data, {compact: true}));
+      const data = JSON.parse(xml2json(response.data, {compact: true}));
 
-    const itemData = data.items.item;
+      const itemData = data.items.item;
 
-    console.log(itemData);
+      console.log(itemData);
+
+      setBoardGameInfo(itemData);
+    }
+    catch (error)
+    {
+      setBoardGameInfo(undefined);
+
+      throw error;
+    }
   }
 }
 
