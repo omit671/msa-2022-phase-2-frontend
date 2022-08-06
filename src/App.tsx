eea3f801e8ed8934752168c1dcbc3c514b3e9174
omit671 <omit671@aucklanduni.ac.nz>
2022-08-06 17:10:41 +1200

    Setup basic BoardGameGeek API querier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from "axios";
+
+const BGG_THING_API_URL = "https://boardgamegeek.com/xmlapi2/thing";
 
 function App() {
+  const [boardGameID, setBoardGameID] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Board Game Search</h1>
+
+        <div>
+          <label>Board Game ID</label>
+          <br />
+          <input
+              type="text"
+              id="board-game-id"
+              name="board-game-name"
+              onChange={async event => setBoardGameID(event.target.value)}
+          />
+          <br />
+          <button onClick={queryAPI}>Search</button>
+        </div>
+      </div>
   );
+
+  async function queryAPI() {
+    const response = await axios.get(BGG_THING_API_URL + "?id=" + boardGameID);
+
+    console.log(response.data);
+  }
 }
 
 export default App;
